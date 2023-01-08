9958e9ba322f7d09a20bd7d3bf3cb2ed744b698d
0xean <0xean.eth@gmail.com>
2023-01-08 08:44:46 -0700

    working first mgiration

--- db/001_initial.sql
+++ db/001_initial.sql
@@ -6,29 +6,29 @@ create table tokens
     name     text   not null,
     symbol   text   not null,
     chain    text   not null,
-    genesis_block numeric check ( genesis_block >= 0 )
+    genesis_block numeric check ( genesis_block >= 0 ),
     height    numeric not null check ( height >= 0 )
 );
 
 -- mainnet 
   -- fox
-insert into tokens values ("0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d", "FOX", "FOX", "ETH", 7446830, 0)
+insert into tokens values ('0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d', 'FOX', 'FOX', 'ETH', 7446830, 0);
   -- foxy
-insert into tokens values ("0xDc49108ce5C57bc3408c3A5E95F3d864eC386Ed3", "FOX Yieldy", "FOXy", "ETH", 14476930, 0)
+insert into tokens values ('0xDc49108ce5C57bc3408c3A5E95F3d864eC386Ed3', 'FOX Yieldy', 'FOXy', 'ETH', 14476930, 0);
   -- fox <> ETH LP
-insert into tokens values ("0x470e8de2eBaef52014A47Cb5E6aF86884947F08c", "UNI-V2", "Uniswap V2", "ETH", 10104463, 0)
+insert into tokens values ('0x470e8de2eBaef52014A47Cb5E6aF86884947F08c', 'UNI-V2', 'Uniswap V2', 'ETH', 10104463, 0);
   -- tfox
-insert into tokens values ("0x808d3e6b23516967ceae4f17a5f9038383ed5311", "TokemaktFOX", "tFOX", "ETH", 13690577, 0)
+insert into tokens values ('0x808d3e6b23516967ceae4f17a5f9038383ed5311', 'TokemaktFOX', 'tFOX', 'ETH', 13690577, 0);
   -- scfox
-insert into tokens values ("0x04979ccCC2F854E167DAAd0AE095Da49eAE4842E", "FOX $0.80 Success Token March 2024", "tFOX", "ETH", 13690577, 0)
+insert into tokens values ('0x04979ccCC2F854E167DAAd0AE095Da49eAE4842E', 'FOX $0.80 Success Token March 2024', 'tFOX', 'ETH', 13690577, 0);
 
 -- gnosis
   -- fox
-insert into tokens values ("0x21a42669643f45bc0e086b8fc2ed70c23d67509d", "FOX on xDai", "FOX", "GNO", 16988521, 0)
+insert into tokens values ('0x21a42669643f45bc0e086b8fc2ed70c23d67509d', 'FOX on xDai', 'FOX', 'GNO', 16988521, 0);
 
 -- Polygon
   -- fox
-insert into tokens values ("0x65a05db8322701724c197af82c9cae41195b0aa8", "FOX on xDai", "FOX", "POLY", 14283484 , 0)
+insert into tokens values ('0x65a05db8322701724c197af82c9cae41195b0aa8', 'FOX', 'FOX', 'POLY', 14283484 , 0);
 
 create table staking
 (
@@ -37,16 +37,13 @@ create table staking
       primary key,
     contract_name   text   not null,
     chain    text   not null,
-    genesis_block numeric check ( genesis_block >= 0 )
+    genesis_block numeric check ( genesis_block >= 0 ),
     height    numeric not null check ( height >= 0 )
-)
+);
 
 -- mainnet 
   -- LP stakingrewards
-insert into staking values ("0xc14eaa8284feff79edc118e06cadbf3813a7e555", "stakingrewards", "ETH", 15941059, 0)
-
-
-
+insert into staking values ('0xc14eaa8284feff79edc118e06cadbf3813a7e555', 'stakingrewards', 'ETH', 15941059, 0);
 
 ---- create above / drop below ----
 -- undo --
