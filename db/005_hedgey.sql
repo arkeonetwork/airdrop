1aaf118f80e483a88aa930ec63c229ebc90f2282
adam <asamere@gmail.com>
2023-01-17 11:49:13 -0800

    fix db migrations

--- db/005_hedgey.sql
+++ db/005_hedgey.sql
@@ -1,7 +1,7 @@
-insert into staking_contracts(address, contract_name, chain, genesis_block, height) values 
- ('0x2aa5d15eb36e5960d056e8fea6e7bb3e2a06a351', 'hedgeyNFT', 'ETH', 14496728, 0),
- ('0x2aa5d15eb36e5960d056e8fea6e7bb3e2a06a351', 'hedgeyNFT', 'GNO', 21401247 , 0);
-
+-- insert into staking_contracts(address, contract_name, chain, genesis_block, height) values 
+--  ('0x2aa5d15eb36e5960d056e8fea6e7bb3e2a06a351', 'hedgeyNFT', 'ETH', 14496728, 0),
+--  ('0x2aa5d15eb36e5960d056e8fea6e7bb3e2a06a351', 'hedgeyNFT', 'GNO', 21401247 , 0);
+select 1;
 ---- create above / drop below ----
 -- undo --
 delete from staking_events where staking_contract = '0x2aa5d15eb36e5960d056e8fea6e7bb3e2a06a351';
