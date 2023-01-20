5e7c19401333b20f3ff039c0ae60ef848eadaa06
adam <asamere@gmail.com>
2023-01-19 19:00:29 -0800

    quiet denomAmt log, remove commented

--- indexer/cosmos.go
+++ indexer/cosmos.go
@@ -172,6 +172,7 @@ func (c *CosmosIndexer) handleStakingTx(height int64, tx tmtypes.Tx, txResult *a
 			if stakingEvt.EventType == "redelegate" {
 				unbondEvt := types.CosmosStakingEvent{
 					EventType:   stakingEvt.EventType,
+					EventIndex:  0, // using zero here for extra row to maintain uniqueness
 					Delegator:   stakingEvt.Delegator,
 					Validator:   stakingEvt.srcValidator,
 					Chain:       c.chain.Name,
@@ -244,7 +245,7 @@ func parseAmount(in string, decimals uint8) (asset string, amount float64, err e
 	amt = amt[:i]
 	asset = in[i:]
 	samt := string(amt)
-	log.Infof("%s %s", samt, asset)
+	log.Debugf("%s %s", samt, asset)
 
 	iamt, ok := new(big.Int).SetString(samt, 10)
 	if !ok {
@@ -255,152 +256,3 @@ func parseAmount(in string, decimals uint8) (asset string, amount float64, err e
 	amount = utils.BigIntToFloat(iamt, uint8(decimals))
 	return
 }
-
-/*
-type txResponseWrapper interface {
-	getResponseDeliverTx() *abcitypes.ResponseDeliverTx
-	getTx() tmtypes.Tx
-}
-type baseTxResponseWrapper struct {
-	tx       tmtypes.Tx
-	txResult *abcitypes.ResponseDeliverTx
-}
-
-func (w baseTxResponseWrapper) getTx() tmtypes.Tx {
-	return w.tx
-}
-
-func (w baseTxResponseWrapper) getResponseDeliverTx() *abcitypes.ResponseDeliverTx {
-	return w.txResult
-}
-
-type delegateTxResponse struct {
-	baseTxResponseWrapper
-}
-type undelegateTxResponse struct {
-	baseTxResponseWrapper
-}
-type redelegateTxResponse struct {
-	baseTxResponseWrapper
-}
-type unbondTxResponse struct {
-	baseTxResponseWrapper
-}
-
-func (c *CosmosIndexer) handleStakingEvents(height int64, txResponses map[string]txResponseWrapper) error {
-	stakingEvents := make([]*types.CosmosStakingEvent, 0, len(txResponses))
-	for _, txw := range txResponses {
-		dumpTxResult(txw.getResponseDeliverTx())
-		var err error
-		var stakingEvent *types.CosmosStakingEvent
-		switch v := txw.(type) {
-		case delegateTxResponse:
-			stakingEvent, err = c.handleDelegate(height, v)
-		case undelegateTxResponse:
-			err = handleUndelegate(height, v)
-		case redelegateTxResponse:
-			err = handleRedelegate(height, v)
-		case unbondTxResponse:
-			err = handleUnbond(height, v)
-		}
-		if err != nil {
-			log.Errorf("error handling %s staking event: %+v", txw.getResponseDeliverTx().GetInfo(), err)
-			continue
-		}
-		if stakingEvent == nil {
-			log.Warnf("no staking event produced for %s", hashTx(txw.getTx()))
-			continue
-		}
-		stakingEvents = append(stakingEvents, stakingEvent)
-	}
-	if len(stakingEvents) > 0 {
-		log.Infof("found %d staking events to write at height %d", len(stakingEvents), height)
-		if err := c.db.InsertStakingEvents(stakingEvents); err != nil {
-			return errors.Wrapf(err, "error inserting staking events at height %d, abort", height)
-		}
-	}
-
-	return nil
-}
-
-func (c *CosmosIndexer) handleDelegate(height int64, d delegateTxResponse) (*types.CosmosStakingEvent, error) {
-	tx := d.tx
-	txHash := hashTx(tx)
-	log.Infof("tx %s has delegate event", txHash)
-	txResult := d.txResult
-
-	attribMap := make(map[string]string, 64)
-	evtsSequenced := make([]abcitypes.Event, len(txResult.GetEvents()))
-	evtsSeq := 0
-	for _, evt := range txResult.GetEvents() {
-		switch evt.GetType() {
-		case "delegate":
-			evtsSequenced[evtsSeq] = evt
-			evtsSeq++
-			// for _, attrib := range evt.GetAttributes() {
-			// 	attribMap[string(attrib.GetKey())] = string(attrib.GetValue())
-			// }
-		// case "transfer":
-		// 	for _, attrib := range evt.GetAttributes() {
-		// 		attribMap[string(attrib.GetKey())] = string(attrib.GetValue())
-		// 	}
-		case "message":
-			m := make(map[string]string, len(evt.GetAttributes()))
-			for _, attr := range evt.GetAttributes() {
-				m[string(attr.GetKey())] = string(attr.GetValue())
-			}
-			if module, ok := m["module"]; ok && module == "staking" {
-				if delegator, ok := m["sender"]; ok {
-					log.Infof("adding delegate event delegator %s", delegator)
-					// log.Infof("setting module_staking_delegator to %s", delegator)
-					// attribMap["module_staking_delegator"] = delegator
-					evtsSequenced[evtsSeq] = evt
-					evtsSeq++
-				}
-			}
-		// case "tx":
-		// 	for _, attrib := range evt.GetAttributes() {
-		// 		if string(attrib.GetKey()) == "acc_seq" {
-		// 			spl := strings.Split(string(attrib.GetValue()), "/")
-		// 			attribMap["delegator"] = spl[0]
-		// 			break
-		// 		}
-		// 		attribMap[string(attrib.GetKey())] = string(attrib.GetValue())
-		// 	}
-		default:
-		}
-	}
-	if len(attribMap) < 1 {
-		return nil, fmt.Errorf("no attributes for delegate event, height: %d, tx: %s", height, txHash)
-	}
-	_, amt, err := parseAmount(attribMap["amount"], c.chain.Decimals)
-	if err != nil {
-		log.Errorf("error parsing amount %s: %+v", attribMap["amount"], err)
-	}
-	cse := &types.CosmosStakingEvent{
-		EventType:   "delegate",
-		Chain:       c.chain.Name,
-		BlockNumber: uint64(height),
-		TxHash:      txHash,
-		Delegator:   attribMap["delegator"],
-		Validator:   attribMap["validator"],
-		Value:       amt,
-	}
-
-	return cse, nil
-}
-*/
-
-// func dumpTxResult(r *abcitypes.ResponseDeliverTx) {
-// 	uniq := make(map[string]string, 256)
-// 	for _, evt := range r.Events {
-// 		// log.Infof("evt: %s", evt.GetType())
-// 		for _, attrib := range evt.GetAttributes() {
-// 			// log.Infof("%s : %s", attrib.GetKey(), attrib.GetValue())
-// 			uniq[string(attrib.GetKey())] = string(attrib.GetValue())
-// 		}
-// 	}
-// 	for k, v := range uniq {
-// 		log.Infof("%s : %s", k, v)
-// 	}
-// }
