91db7e00ee4c43ba70a25ce38e74cc9265e5942d
toshiSat <10103480+toshiSat@users.noreply.github.com>
2024-06-04 12:54:53 +0500

    Update block numbers

--- db/011_thorchain.sql
+++ db/011_thorchain.sql
@@ -1,5 +1,5 @@
 insert into chains(name,rpc_url,snapshot_start_block,snapshot_end_block,decimals)
-values ('THOR','https://daemon.thorchain.shapeshift.com:443/rpc',8779501,9779501,8);
+values ('THOR','https://daemon.thorchain.shapeshift.com:443/rpc',8336490,15475617,8);
 
 ---- create above / drop below ----
 delete from chains where name = 'THOR';
--- db/014_osmo.sql
+++ db/014_osmo.sql
@@ -1,5 +1,5 @@
 insert into chains(name,rpc_url,lcd_url,snapshot_start_block,snapshot_end_block,decimals)
-values ('OSMO','https://daemon.osmosis.shapeshift.com:443/rpc','https://daemon.osmosis.shapeshift.com:443/lcd',7117606,8049108,6);
+values ('OSMO','https://daemon.osmosis.shapeshift.com:443/rpc','https://daemon.osmosis.shapeshift.com:443/lcd',7009135,14796105,6);
 
 ---- create above / drop below ----
 delete from chains where name = 'OSMO';
